89a7b509aebc86a0df444884ab51303831c6b0c3
kenyu <kenyujp2021@gmail.com>
2022-08-08 20:17:10 +0900

    fix navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import {Navbar, NavbarBrand} from 'reactstrap';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 
 function App() {
   return (
     <div className="App">
       <Navbar dark color='primary'>
-        <div className='container'>
-          <NavbarBrand href='/' > My React App</NavbarBrand>
-        </div>
-        <Menu/>
+        <NavbarBrand href='/' > My React App</NavbarBrand>
       </Navbar>
+      <Menu/>
     </div>
   );
 }
